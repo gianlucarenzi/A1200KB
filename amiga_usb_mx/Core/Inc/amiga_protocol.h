fb3dd5aaf0cb1c4d1e85f8ecd5dcdb9d9145d8a8
RetroBitLab <icjtqr@gmail.com>
2022-01-21 19:35:31 +0100

    First attempt to drive Amiga Keyboard pins

---
+++ amiga_usb_mx/Core/Inc/amiga_protocol.h
@@ -0,0 +1,19 @@
+#ifndef __AMIGA_PROTOCOL_INCLUDED__
+#define __AMIGA_PROTOCOL_INCLUDED__
+
+#include <stdint.h>
+#include <inttypes.h>
+#include <stdbool.h>
+#include "keyboard.h"
+#include "keycode.h"
+#include "report.h"
+#include "config.h"
+#include "amiga_config.h"
+#include "debug.h"
+
+extern void amiga_protocol_init(void);
+extern void amiga_protocol_send(keyevent_t event);
+extern void amiga_protocol_reset(void);
+extern bool amiga_protocol_is_reset(void);
+
+#endif
