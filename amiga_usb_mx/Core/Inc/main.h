a823ce452f7f8aa63338e03e871e608708ea1b64
RetroBitLab <icjtqr@gmail.com>
2022-01-21 12:48:47 +0100

    Updated with all used pins

--- amiga_usb_mx/Core/Inc/main.h
+++ amiga_usb_mx/Core/Inc/main.h
@@ -34,35 +34,33 @@ extern "C" {
 void Error_Handler(void);
 
 /* Private defines -----------------------------------------------------------*/
-#define K0_Pin GPIO_PIN_4
-#define K0_GPIO_Port GPIOE
 
-#define ROW0_Pin GPIO_PIN_5
-#define ROW0_GPIO_Port GPIOE
-#define ROW1_Pin GPIO_PIN_6
-#define ROW1_GPIO_Port GPIOE
-#define ROW2_Pin GPIO_PIN_7
-#define ROW2_GPIO_Port GPIOE
-#define ROW3_Pin GPIO_PIN_8
-#define ROW3_GPIO_Port GPIOE
-#define ROW4_Pin GPIO_PIN_9
-#define ROW4_GPIO_Port GPIOE
-#define ROW5_Pin GPIO_PIN_10
-#define ROW5_GPIO_Port GPIOE
-#define ROW6_Pin GPIO_PIN_11
-#define ROW6_GPIO_Port GPIOE
-#define ROW7_Pin GPIO_PIN_12
-#define ROW7_GPIO_Port GPIOE
-#define ROW8_Pin GPIO_PIN_13
-#define ROW8_GPIO_Port GPIOE
-#define ROW9_Pin GPIO_PIN_14
-#define ROW9_GPIO_Port GPIOE
-#define ROW10_Pin GPIO_PIN_15
-#define ROW10_GPIO_Port GPIOE
-#define ROW11_Pin GPIO_PIN_0
-#define ROW11_GPIO_Port GPIOD
-#define ROW12_Pin GPIO_PIN_1
-#define ROW12_GPIO_Port GPIOD
+#define ROW0_Pin GPIO_PIN_0
+#define ROW0_GPIO_Port GPIOB
+#define ROW1_Pin GPIO_PIN_1
+#define ROW1_GPIO_Port GPIOB
+#define ROW2_Pin GPIO_PIN_2
+#define ROW2_GPIO_Port GPIOB
+#define ROW3_Pin GPIO_PIN_3
+#define ROW3_GPIO_Port GPIOB
+#define ROW4_Pin GPIO_PIN_4
+#define ROW4_GPIO_Port GPIOB
+#define ROW5_Pin GPIO_PIN_5
+#define ROW5_GPIO_Port GPIOB
+#define ROW6_Pin GPIO_PIN_6
+#define ROW6_GPIO_Port GPIOB
+#define ROW7_Pin GPIO_PIN_7
+#define ROW7_GPIO_Port GPIOB
+#define ROW8_Pin GPIO_PIN_8
+#define ROW8_GPIO_Port GPIOB
+#define ROW9_Pin GPIO_PIN_9
+#define ROW9_GPIO_Port GPIOB
+#define ROW10_Pin GPIO_PIN_10
+#define ROW10_GPIO_Port GPIOB
+#define ROW11_Pin GPIO_PIN_12
+#define ROW11_GPIO_Port GPIOB
+#define ROW12_Pin GPIO_PIN_13
+#define ROW12_GPIO_Port GPIOB
 
 #define COL0_Pin GPIO_PIN_0
 #define COL0_GPIO_Port GPIOC
@@ -81,33 +79,33 @@ void Error_Handler(void);
 #define COL7_Pin GPIO_PIN_7
 #define COL7_GPIO_Port GPIOC
 #define COL8_Pin GPIO_PIN_8
-#define COL8_GPIO_Port GPIOD
+#define COL8_GPIO_Port GPIOC
 #define COL9_Pin GPIO_PIN_9
-#define COL9_GPIO_Port GPIOD
+#define COL9_GPIO_Port GPIOC
 #define COL10_Pin GPIO_PIN_10
-#define COL10_GPIO_Port GPIOD
+#define COL10_GPIO_Port GPIOC
 #define COL11_Pin GPIO_PIN_11
-#define COL11_GPIO_Port GPIOD
+#define COL11_GPIO_Port GPIOC
 #define COL12_Pin GPIO_PIN_12
-#define COL12_GPIO_Port GPIOD
+#define COL12_GPIO_Port GPIOC
 #define COL13_Pin GPIO_PIN_13
-#define COL13_GPIO_Port GPIOD
+#define COL13_GPIO_Port GPIOC
 #define COL14_Pin GPIO_PIN_14
-#define COL14_GPIO_Port GPIOD
+#define COL14_GPIO_Port GPIOC
 #define COL15_Pin GPIO_PIN_15
-#define COL15_GPIO_Port GPIOD
+#define COL15_GPIO_Port GPIOC
 
-#define DEBUG_TX_Pin GPIO_PIN_9
+#define DEBUG_TX_Pin GPIO_PIN_2
 #define DEBUG_TX_GPIO_Port GPIOA
-#define DEBUG_RX_Pin GPIO_PIN_10
+#define DEBUG_RX_Pin GPIO_PIN_3
 #define DEBUG_RX_GPIO_Port GPIOA
 
-#define KB_RST_Pin GPIO_PIN_0
-#define KB_RST_GPIO_Port GPIOA
-#define KB_CLK_Pin GPIO_PIN_1
-#define KB_CLK_GPIO_Port GPIOA
-#define KB_DAT_Pin GPIO_PIN_2
+#define KB_DAT_Pin GPIO_PIN_5
 #define KB_DAT_GPIO_Port GPIOA
+#define KB_CLK_Pin GPIO_PIN_6
+#define KB_CLK_GPIO_Port GPIOA
+#define KB_RST_Pin GPIO_PIN_7
+#define KB_RST_GPIO_Port GPIOA
 
 /**
  * The keyboard can have up to 5 leds:
