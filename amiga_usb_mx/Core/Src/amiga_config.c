06a146ec58cd166b1b2cb5f2e6d90bd3dcb72a2f
Gianluca Renzi <gianlucarenzi@eurek.it>
2025-11-19 10:18:23 +0100

    Updated layout

--- amiga_usb_mx/Core/Src/amiga_config.c
+++ amiga_usb_mx/Core/Src/amiga_config.c
@@ -61,24 +61,25 @@ static int debuglevel = DBG_INFO;
 	 * 
 	 */
 
+/* We have max 13 rows and 16 columns. So we need to arrange a different layout here */
 const uint8_t keymaps[][KEYBOARD_ROWS][KEYBOARD_COLUMNS] = {
 	[0] = {
-		{ KC_ESC,     KC_KP_LPAREN,  KC_F1,  KC_F2,  KC_F3,  KC_F4,    KC_F5,  KC_KP_RPAREN,  KC_F6,    KC_PSLS,  KC_F7,    KC_F8,       KC_F9,      KC_F10,   KC_HELP,  KC_NO },
-		{ KC_GRV,     KC_1,          KC_2,   KC_3,   KC_4,   KC_5,     KC_6,   KC_7,          KC_8,     KC_9,     KC_0,     KC_MINS,     KC_EQL,     KC_BSLS,  KC_UP,    KC_NO },
-		{ KC_TAB,     KC_Q,          KC_W,   KC_E,   KC_R,   KC_T,     KC_Y,   KC_U,          KC_I,     KC_O,     KC_P,     KC_LBRC,     KC_RBRC,    KC_ENT,   KC_LEFT,  KC_NO },
-		{ KC_CAPS,    KC_A,          KC_S,   KC_D,   KC_F,   KC_G,     KC_H,   KC_J,          KC_K,     KC_L,     KC_SCLN,  KC_QUOT,     KC_SPARE_1, KC_DEL,   KC_RIGHT, KC_NO },
-		{ KC_SPARE_2, KC_Z,          KC_X,   KC_C,   KC_V,   KC_B,     KC_N,   KC_M,          KC_COMM,  KC_DOT,   KC_SLSH,  KC_SPARE_3,  KC_SPC,     KC_BSPC,  KC_DOWN,  KC_NO },
-		{ KC_PAST,    KC_PPLS,       KC_P9,  KC_P6,  KC_P3,  KC_PCMM,  KC_P8,  KC_P5,         KC_P2,    KC_PENT,  KC_P7,    KC_P4,       KC_P1,      KC_P0,    KC_PMNS,  KC_NO },
-		{ KC_NO,      KC_NO,         KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,  KC_NO,         KC_NO,    KC_NO,    KC_NO,    KC_NO,       KC_NO,      KC_NO,    KC_NO,    KC_RSFT },
-		{ KC_NO,      KC_NO,         KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,  KC_NO,         KC_NO,    KC_NO,    KC_NO,    KC_NO,       KC_NO,      KC_NO,    KC_NO,    KC_RALT },
-		{ KC_NO,      KC_NO,         KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,  KC_NO,         KC_NO,    KC_NO,    KC_NO,    KC_NO,       KC_NO,      KC_NO,    KC_NO,    KC_RGUI },
-		{ KC_NO,      KC_NO,         KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,  KC_NO,         KC_NO,    KC_NO,    KC_NO,    KC_NO,       KC_NO,      KC_NO,    KC_NO,    KC_LCTRL },
-		{ KC_NO,      KC_NO,         KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,  KC_NO,         KC_NO,    KC_NO,    KC_NO,    KC_NO,       KC_NO,      KC_NO,    KC_NO,    KC_LSFT },
-		{ KC_NO,      KC_NO,         KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,  KC_NO,         KC_NO,    KC_NO,    KC_NO,    KC_NO,       KC_NO,      KC_NO,    KC_NO,    KC_LALT },
-		{ KC_NO,      KC_NO,         KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,  KC_NO,         KC_NO,    KC_NO,    KC_NO,    KC_NO,       KC_NO,      KC_NO,    KC_NO,    KC_LGUI },
+		{ KC_ESC,    KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,    KC_F7, KC_F8, KC_F9,   KC_F10,  KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_NO,   }, // ROW0
+		{ KC_NO,     KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_NO,   }, // ROW1 ---> Logical Upper ROW0
+		{ KC_GRV,    KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,     KC_7,  KC_8,  KC_9,    KC_0,    KC_MINS,    KC_EQL,  KC_NO,   KC_BSLS, KC_DEL,  }, // ROW2
+		{ KC_NO,     KC_HELP,  KC_PLPR, KC_PRPR, KC_PSLS, KC_PAST, KC_NO,    KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_NO,   }, // ROW3 ---> Logical ROW1
+		{ KC_KC_TAB, KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,     KC_U,  KC_I,  KC_O,    KC_P,    KC_LBRC,    KC_RBRC, KC_NO,   KC_ENT,  KC_NO,   }, // ROW4
+		{ KC_NO,     KC_NO,    KC_P7,   KC_P8,   KC_P9,   KC_PMNS, KC_NO,    KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_NO,   }, // ROW5 ---> Logical ROW2
+		{ KC_LCTL,   KC_CLCK,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,  KC_J,  KC_K,    KC_L,    KC_SCLN,    KC_QUOT, KC_INT1, KC_NO,   KC_NO,   }, // ROW6
+		{ KC_UP,     KC_NO,    KC_P4,   KC_P5,   KC_P6,   KC_PPLS, KC_NO,    KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_NO,   }, // ROW7 ---> Logical ROW3
+		{ KC_LSFT,   KC_INT2,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,  KC_M,  KC_COMM, KC_DOT,  KC_SLSH,    KC_NO,   KC_NO,   KC_RSFT, KC_LEFT, }, // ROW8
+		{ KC_DOWN,   KC_RIGHT, KC_P1,   KC_P2,   KC_P3,   KC_PENT, KC_NO,    KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_NO,   }, // ROW9 ---> Logical ROW4
+		{ KC_NO,     KC_LALT,  KC_LGUI, KC_NO,   KC_NO,   KC_NO,   KC_SPACE, KC_NO, KC_NO, KC_RGUI, KC_RALT, KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_NO,   }, // ROW10
+		{ KC_NO,     KC_NO,    KC_P0,   KC_NO,   KC_PDOT, KC_NO,   KC_NO,    KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_NO,   }, // ROW11 ---> Logical ROW5
 	},
 };
 
+/* We are configuring ALL ROWS and ALL COLUMNS anyway */.
 gpioPort_t lut_row[ KEYBOARD_ROWS ] = {
 	{ .port = ROW0_GPIO_Port,  .pin = ROW0_Pin },
 	{ .port = ROW1_GPIO_Port,  .pin = ROW1_Pin },
